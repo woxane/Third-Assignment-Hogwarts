a5367648b2c538e94d6f953ae72a1d197dd88456
“MMD_PROJECT” <mmdvioleti@gmail.com>
2024-03-07 12:03:14 +0330

    forgot to add on Main

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+/*
+    TODO: Import all the classes that you have defined and make use of them to build the program.
+ */
+
+
+public class Main {
+    public static void main(String[] args) {
+        // TODO: Program starts from here
+    }
+
+    public static void runMenu() {
+        // TODO: Menu will be shown here...
+    }
+}
