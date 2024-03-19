18d5aedb25710ba49160e1f670f218b79326c79c
woxane <mohsen.minavand1384@gmail.com>
2024-03-19 21:08:25 +0330

    Refactor code base to support courseId

--- src/main/java/Rate.java
+++ src/main/java/Rate.java
@@ -3,11 +3,13 @@ import java.util.UUID;
 public class Rate {
     final UUID raterId;
     final UUID subjectId;
+    final UUID courseId;
     double rating;
 
-    Rate (UUID raterId , UUID subjectId , double rating) {
+    Rate (UUID raterId , UUID subjectId , UUID courseId , double rating) {
         this.raterId = raterId;
         this.subjectId = subjectId;
+        this.courseId = courseId;
         this.rating = rating;
 
     }
