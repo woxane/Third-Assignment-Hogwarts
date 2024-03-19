08b9e51dfda73c3802243b5bb261ab3d29b332ac
woxane <mohsen.minavand1384@gmail.com>
2024-03-19 19:27:07 +0330

    Implement Student class constructor

---
+++ src/main/java/Student.java
@@ -0,0 +1,6 @@
+public class Student extends Account {
+    static final String role = "Student";
+    Student(String username, String password, String role) {
+        super(username, password, role);
+    }
+}
