6dab7850db1367058a48f61c6b69480857046863
woxane <mohsen.minavand1384@gmail.com>
2024-03-19 22:35:20 +0330

    Implement basics of Assistant constructor

---
+++ src/main/java/Assistant.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Assistant extends Account {
+    static final String role = "Admin";
+    Assistant(String username, String password, String role) {
+        super(username, password, role);
+    }
+   
+}
