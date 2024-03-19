d9b9f09a9e3c6643dfb54d599fdc8f850501feda
woxane <mohsen.minavand1384@gmail.com>
2024-03-19 20:56:15 +0330

    utilize getRating in Hogwarts class

--- src/main/java/Hogwarts.java
+++ src/main/java/Hogwarts.java
@@ -4,19 +4,20 @@ public class Hogwarts {
     static ArrayList<Teacher> teachers = new ArrayList<>();
     static ArrayList<Student> students = new ArrayList<>();
     static ArrayList<Course> courses = new ArrayList<>();
+    static ArrayList<Rate> rates = new ArrayList<>();
 
 
     public void viewAllTeachers() {
         for (Teacher teacher : teachers) {
             System.out.println("Name : " + teacher.username + "\nRatings : " + teacher.rating +
-                    " (with " + teacher.numberOfVotes + " votes)");
+                    " (with " + teacher.getRating() + " votes)");
         }
     }
 
     public void viewAllStudents() {
         for (Student student : students) {
             System.out.println("Name : " + student.username + "\nRatings : " + student.rating +
-                    " (with " + student.numberOfVotes + " votes)");
+                    " (with " + student.getRating() + " votes)");
         }
     }
 
