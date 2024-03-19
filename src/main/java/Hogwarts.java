2d0827c3026d48a68bf653c74ad90888e438b290
woxane <mohsen.minavand1384@gmail.com>
2024-03-19 22:29:46 +0330

    Refactor view functions to prints with number on it

--- src/main/java/Hogwarts.java
+++ src/main/java/Hogwarts.java
@@ -5,25 +5,28 @@ public class Hogwarts {
     static ArrayList<Student> students = new ArrayList<>();
     static ArrayList<Course> courses = new ArrayList<>();
     static ArrayList<Rate> rates = new ArrayList<>();
-
+    static ArrayList<Assistant> Asssistants = new ArrayList<>();
 
     static public void viewAllTeachers() {
+        int i = 1;
         for (Teacher teacher : teachers) {
-            System.out.println("Name : " + teacher.username + "\nRatings : " + teacher.getRating() +
+            System.out.println(i + ") " + "Name : " + teacher.username + "\nRatings : " + teacher.getRating() +
                     " (with " + teacher.rating.size() + " votes)");
         }
     }
 
     static public void viewAllStudents() {
+        int i = 1;
         for (Student student : students) {
-            System.out.println("Name : " + student.username + "\nRatings : " + student.getRating() +
+            System.out.println(i + ") " + "Name : " + student.username + "\nRatings : " + student.getRating() +
                     " (with " + student.rating.size() + " votes)");
         }
     }
 
     static public void viewAllCourses() {
+        int i = 1;
         for (Course course : courses ) {
-            System.out.println("Title : " + course.title + "\nTeacher Name : " + course.teacher +
+            System.out.println(i + ") " + "Title : " + course.title + "\nTeacher Name : " + course.teacher +
                     "\nAssignments : " + course.assignment);
 
         }
