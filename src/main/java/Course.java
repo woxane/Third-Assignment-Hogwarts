7f1c0f982967ab8977a710a5f99fb52e30d5fbeb
woxane <mohsen.minavand1384@gmail.com>
2024-03-19 19:37:02 +0330

    Add lists of students in it

--- src/main/java/Course.java
+++ src/main/java/Course.java
@@ -1,9 +1,11 @@
+import java.util.ArrayList;
 import java.util.UUID;
 
 public class Course {
     final String title;
     final UUID courseID;
     String assignment;
+    static ArrayList<Student> students = new ArrayList<>();
 
     Course (String title) {
         this.title = title;
