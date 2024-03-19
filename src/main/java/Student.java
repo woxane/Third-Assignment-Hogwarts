cbdf68ce2122b8950a989f1d3b2d5f348b5a49c8
woxane <mohsen.minavand1384@gmail.com>
2024-03-20 00:23:06 +0330

    Implement takeCourse function

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,6 +1,38 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Student extends Account {
     static final String role = "Student";
+    ArrayList<Course> courses = new ArrayList<>();
+
     Student(String username, String password, String role) {
         super(username, password, role);
+    }
+    public void takeCourse() {
+        if (Hogwarts.courses.isEmpty()) {
+            System.out.println("Sorry there is no course for now </3 .");
+        } else {
+            System.out.println("We have the above courses .\nChoose one of the above : ");
+            Hogwarts.viewAllCourses();
+
+            Scanner scanner = new Scanner(System.in);
+
+            do {
+                int chosenCourse = scanner.nextInt();
+
+                if (!(chosenCourse >= Hogwarts.courses.size() | chosenCourse <= 0)) {
+                    Course course = Hogwarts.courses.get(chosenCourse - 1);
+                    course.students.add(this);
+                    this.courses.add(course);
+                    System.out.println("Done .");
+                    break;
+                } else {
+                    System.out.println("Please choose one of the above : ");
+                }
+
+            } while (true);
+
+        }
+
     }
 }
