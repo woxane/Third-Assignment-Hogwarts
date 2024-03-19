705a9391f2033e08ac80a7cc695d2f92a87caca5
woxane <mohsen.minavand1384@gmail.com>
2024-03-19 16:26:52 +0330

    Implement basics of Course constructor

--- src/main/java/Course.java
+++ src/main/java/Course.java
@@ -1,2 +1,13 @@
+import java.util.UUID;
+
 public class Course {
+    final String title;
+    final UUID courseID;
+    String assignment;
+
+    Course (String title) {
+        this.title = title;
+        this.courseID = UUID.randomUUID();
+        this.assignment = "";
+    }
 }
